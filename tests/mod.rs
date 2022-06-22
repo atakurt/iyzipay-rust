4f61da188d5b859c2243c95911ba6ac7e37a581c
İsmail Ata Kurt <ismaila.kurt@sahibinden.com>
2022-06-22 21:00:47 +0300

    github workflow

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,9 +2,9 @@ use iyzipay_rust::options::Options;
 
 pub fn get_test_options() -> Options {
     let mut options = Options::new();
-    options.set_api_key(env!("api_key"));
-    options.set_secret_key(env!("secret_key"));
-    options.set_base_url(env!("base_url"));
+    options.set_api_key(env!("API_KEY", "env API_KEY not set"));
+    options.set_secret_key(env!("SECRET_KEY", "env SECRET_KEY not set"));
+    options.set_base_url(env!("BASE_URL", "env BASE_URL not set"));
     options
 }
 
